3aa362d5d3d056c098afd068f5069f051e51a0d1
Rafał Wawrzkowicz <rafal.wawrzkowicz@intive.com>
2022-02-28 13:00:04 +0100

    feat: hello kotlin example
    - in, for each, constant

--- src/main/kotlin/HelloKotlin.kt
+++ src/main/kotlin/HelloKotlin.kt
@@ -1,10 +1,20 @@
 class HelloKotlin {
-    fun function (): Unit {
-        val iloscKotow = 17
-        println("alamakota $iloscKotow")
+    val predefinedCats = arrayListOf<String>("a", "b", "c");
+
+    fun duplicate (catsAmount: Int, whoOwnsCats: String): Unit {
+        val duplicateBy = 2
+        println("$whoOwnsCats ma kotow $catsAmount * 2")
+    }
+
+    fun checkIfCatsIn(cats: Array<String>): Boolean {
+        var result = false;
+        for (cat in cats) {
+            result = result && cat in predefinedCats
+        }
+        return result;
     }
 }
 
 fun main() {
-    HelloKotlin().function();
+    HelloKotlin().duplicate(10, "Ala");
 }
